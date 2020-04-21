384f7634986af55bf792ddf4aac3535bf7e33911
herison santos <herisonascimento@gmail.com>
2020-04-20 23:15:33 -0300

    atividade 03 herison

--- app/src/main/java/br/com/mariojp/exercise3/Tarefa.java
+++ app/src/main/java/br/com/mariojp/exercise3/Tarefa.java
@@ -1,19 +1,43 @@
 package br.com.mariojp.exercise3;
 
-public class Tarefa {
-    private String descricao;
+public class Tarefa  implements Comparable<Tarefa> {
+    private String desc;
     private int prioridade;
 
-    public Tarefa(String descricao, int prioridade) {
-        this.descricao = descricao;
+    public Tarefa(String desc, int prioridade) {
+        this.desc = desc;
         this.prioridade = prioridade;
     }
 
     public String getDescricao() {
-        return descricao;
+        return desc;
     }
 
     public int getPrioridade() {
         return prioridade;
+    }
+
+    public int compareTo(Tarefa tarefa) {
+        if (this.prioridade < tarefa.getPrioridade()) {
+            return -1;
+        }
+        if (this.prioridade > tarefa.getPrioridade()) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Tarefa other = (Tarefa) obj;
+        return this.desc.equals(other.desc);
+    }
+
+    public boolean verificarPrioridade(Tarefa tarefa) {
+        if(tarefa.getPrioridade() <= 0 || tarefa.getPrioridade() > 10) {
+            return true;
+        }else {
+            return false;
+        }
     }
 }
